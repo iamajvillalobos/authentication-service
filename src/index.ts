7f9615014226b4caf1d4188464513f6e737b2669
AJ Villalobos <hello@ajvillalobos.com>
2024-06-17 16:07:00 +0800

    Add authentication logic

--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,18 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+export interface Env {
+	API_AUTH_KEY: String;
+}
+
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+		const api_key = request.headers.get('x-api-auth-key');
+
+		if (api_key === env.API_AUTH_KEY) {
+			return new Response('Authenticated', { status: 200 });
+		}
+
+		return new Response('Unauthorized', { status: 401 });
 	},
 } satisfies ExportedHandler<Env>;
